7e634d412d7bdc0b6498ad152818366723572662
Kyle Rockman <kyle.rockman@mac.com>
2014-05-20 08:50:45 -0500

    setup.py configuration for jenkins testing

---
+++ kyleandemily/__init__.py
@@ -0,0 +1,7 @@
+__import__('pkg_resources').declare_namespace(__name__)
+
+__description__ = ''
+__author__ = 'Kyle & Emily Rockman'
+__author_email__ = ''
+__url__ = 'https://github.com/ehayne/kyleandemily'
+__version__ = '0.0.1'
--- kyleandemily/wsgi.py
+++ kyleandemily/wsgi.py
@@ -9,9 +9,16 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 import sys
 
-activate_this = '/Users/zoeserver/.pyenv/versions/kyleandemily/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
-sys.path.append('/Volumes/STORAGE/Web/kyleandemily')
+try:
+    activate_this = os.path.join('/Volumes', 'STORAGE', 'Web', 'kyleandemily', '.virtualenv', 'bin', 'activate_this.py')
+    execfile(activate_this, dict(__file__=activate_this))
+    sys.path.append('/Volumes/STORAGE/Web/kyleandemily')
+except:
+    activate_this = os.path.join(os.getcwd(), '.virtualenv', 'bin', 'activate_this.py')
+    execfile(activate_this, dict(__file__=activate_this))
+    sys.path.append(os.getcwd())
+    
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kyleandemily.settings")
 
 from django.core.wsgi import get_wsgi_application
