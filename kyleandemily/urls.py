0e69d6f28da26cf629a291441462b52b381f1b5e
ehayne <ebuschang@gmail.com>
2014-05-30 17:44:47 -0500

    added under construction page

--- kyleandemily/urls.py
+++ kyleandemily/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+import kyleandemily.views as views
 import kyleandemily.wedding.urls
 
 urlpatterns = patterns('',
-
-    url(r'^$', include('kyleandemily.wedding.urls')),  # change in the future if we change site
+   url(r'^$',
+       views.under_construction,
+       name='under_construction'),
+#    url(r'^$', include('kyleandemily.wedding.urls')),  # change in the future if we change site
     url(r'^admin/', include(admin.site.urls)),
     url(r'^wedding/', include('kyleandemily.wedding.urls')),
 )
---
+++ kyleandemily/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
+
+
+
+def under_construction(request):
+    template = loader.get_template('under_construction.html')
+    context = RequestContext(request, {
+        'latest_question_list': 'x',
+    })
+    return HttpResponse(template.render(context))
