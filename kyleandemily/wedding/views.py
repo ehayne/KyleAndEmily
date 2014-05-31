1c9d919f547f7ea408eff7232831cbdd55c068f1
ehayne <ebuschang@gmail.com>
2014-05-31 00:32:18 -0500

    added a few photos and scoped out the home page and about us

--- kyleandemily/wedding/views.py
+++ kyleandemily/wedding/views.py
@@ -1,13 +1,20 @@
+from datetime import date
+
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render_to_response
 
-
+WEDDING_DATE = date(2015, 04, 10)
 
 def home(request):
+
+    today = date.today()  # TODO: uses UTC date, needs to use local timezone date
+
+    time_until_wedding = WEDDING_DATE - today
+
     template = loader.get_template('home.html')
     context = RequestContext(request, {
-        'latest_question_list': 'x',
+        'days_left': time_until_wedding.days,
     })
     return HttpResponse(template.render(context))
 
@@ -20,9 +27,15 @@ def about(request):
 
 
 def gallery(request):
+    img_list = [{'img_path': 'wedding/img/vinyard.jpg',
+    		 'img_text': 'Our first trip to Driftwood Vinyards where Kyle later proposed!'},
+    		 {'img_path': 'wedding/img/engaged.jpg',
+    		 'img_text': 'Right after Kyle proposed!'},
+    ]
+
     template = loader.get_template('gallery.html')
     context = RequestContext(request, {
-        'latest_question_list': 'x',
+        'img_list': img_list,
     })
     return HttpResponse(template.render(context))
 
