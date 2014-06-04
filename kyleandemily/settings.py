24875f2373a68c7e8dbf28d793ed2b8604ae27b7
Kyle Rockman <kyle.rockman@mac.com>
2014-06-03 20:49:38 -0500

    adding media url and changing static url

--- kyleandemily/settings.py
+++ kyleandemily/settings.py
@@ -82,10 +82,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'http://media.kyleandemily.com/'
+STATIC_URL = 'http://static.kyleandemily.com/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'http://media.kyleandemily.com/'
 
 try:
     from kyleandemily.local_settings import *
