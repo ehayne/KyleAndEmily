032ca7570e6e582ebfd31fd8fe5a53c5cf5a9e0f
ehayne <ebuschang@gmail.com>
2014-06-13 11:14:09 -0500

    added rest of fancybox static files

--- kyleandemily/base/static/base/js/main.js
+++ kyleandemily/base/static/base/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-		$(".fancybox").fancybox();
+	$(".fancybox").fancybox();
+
+    $(".fancybox").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+//		'speedIn'		:	600,
+//		'speedOut'		:	200,
+		'overlayShow'	:	false
+	});
 });
