d7a6680677589beae9929fe4aca870877836e0fe
Kyle Rockman <kyle.rockman@mac.com>
2014-06-17 18:57:03 -0500

    changing up the order to test the deployment

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@ echo $WORKSPACE
 export PROJECT_NAME=kyleandemily
 export VENV_ROOT=/Users/zoeserver/.virtualenvs/${PROJECT_NAME}/
 export WEB_ROOT=/Volumes/STORAGE/Web/${PROJECT_NAME}
-rm -rvf ${WEB_ROOT}
-mkdir ${WEB_ROOT}
-cp -rv ./* ${WEB_ROOT}
-cd ${WEB_ROOT}
 ln -s ${VENV_ROOT} ./.virtualenv
 . ./.virtualenv/bin/activate
 pip install --upgrade -r ./requirements.txt
 rm -f /usr/local/etc/uwsgi/apps/kyleandemily.ini
 mv ./wsgi.ini /usr/local/etc/uwsgi/apps/kyleandemily.ini
 
+rm -rvf ${WEB_ROOT}
+mkdir ${WEB_ROOT}
+cp -rv ./* ${WEB_ROOT}
+cd ${WEB_ROOT}
 python manage.py collectstatic --noinput
