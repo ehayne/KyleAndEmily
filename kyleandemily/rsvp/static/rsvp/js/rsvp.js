067154d90d97946604e6646d0571888c5481bf91
ehayne <ebuschang@gmail.com>
2014-06-17 16:49:24 -0500

    fixed javascript on rsvp submission

--- kyleandemily/rsvp/static/rsvp/js/rsvp.js
+++ kyleandemily/rsvp/static/rsvp/js/rsvp.js
@@ -7,19 +7,18 @@ $(document).ready(function() {
     $first_name.focus();
 
     $lookup_form.submit(function (event) {
-        console.log('first', $first_name.val())
-        console.log('last', $last_name.val())
+        event.preventDefault();
         $.ajax({
-            url: '/lookup/',
+            url: 'lookup/',
             cache: false,
             data: {
                 'first_name': $first_name.val(),
                 'last_name': $last_name.val()
             },
             success: function (data, status, jqXHR) {
-                $lookup_form.fadeOut(function () {
-                    $container.append(data);
-                });
+                $lookup_form.unbind().submit();
+                $lookup_form.submit();
+
             },
             error: function (jqXHR, status) {
                 var $error = $('<div data-alert class="alert-box alert radius">Sorry, we could not find your invitation</div>');
@@ -29,6 +28,5 @@ $(document).ready(function() {
                 }, 500);
             }
         });
-        event.preventDefault();
     });
 });
