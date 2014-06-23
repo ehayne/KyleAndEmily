eceb17a73ae8f5e85b9a06a197ca378a6c033386
Kyle Rockman <kyle.rockman@mac.com>
2014-06-23 17:12:35 -0500

    fixing the database location

--- kyleandemily/settings.py
+++ kyleandemily/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-DB_ROOT = '/Volumes/STORAGE/Database/kyleandemily'
+DB_ROOT = '/www/database/kyleandemily'
 
 
 
