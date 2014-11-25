e26a87986b8ee1e0656f3be443352852a27d2f1b
Kyle Rockman <kyle.rockman@mac.com>
2014-11-25 07:18:37 -0600

    fixing main page
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- kyleandemily/urls.py
+++ kyleandemily/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url, handler404, handler500
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 
 from django.contrib import admin
@@ -8,15 +9,13 @@ admin.autodiscover()
 
 import kyleandemily.views as views
 
-urlpatterns = patterns('',
-   url(r'^$',
-       views.under_construction,
-       name='under_construction'),
-#    url(r'^$', include('kyleandemily.wedding.urls')),  # change in the future if we change site
+urlpatterns = patterns(
+    '',
+    url(r'^.*$', RedirectView.as_view(url='/wedding', permanent=False), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^wedding/', include('kyleandemily.wedding.urls')),
     url(r'^photologue/', include('photologue.urls')),
-)
+)   
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
