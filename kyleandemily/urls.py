490475607dc4c5ae04aaf30513f49b756e113d64
hayne <ebuschang@gmail.com>
2014-11-26 10:46:18 -0600

    tried to fix construciton page

--- kyleandemily/urls.py
+++ kyleandemily/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url, handler404, handler500
 from django.conf.urls.static import static
-from django.views.generic.base import RedirectView
 
 
 from django.contrib import admin
@@ -11,10 +10,10 @@ import kyleandemily.views as views
 
 urlpatterns = patterns(
     '',
-    url(r'^$',
-           views.under_construction,
-           name='under_construction'),
-    #url(r'^$', RedirectView.as_view(url='/wedding', permanent=False), name='index'),
+    # url(r'^$',
+    #        views.under_construction,
+    #        name='under_construction'),
+    url(r'^', include('kyleandemily.wedding.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^wedding/', include('kyleandemily.wedding.urls')),
     url(r'^photologue/', include('photologue.urls')),
