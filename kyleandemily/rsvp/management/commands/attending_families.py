5679a67a5842b375997bb9fe4a0161898eed20ce
Kyle Rockman <kyle.rockman@mac.com>
2015-02-28 12:51:04 -0600

    fix logic to support csv format
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- kyleandemily/rsvp/management/commands/attending_families.py
+++ kyleandemily/rsvp/management/commands/attending_families.py
@@ -13,14 +13,27 @@ class Command(BaseCommand):
         guest_count = 0
         families = []
         for inv in Invitation.objects.filter(responded=True):
+            attending_count = 0
             family = []
             for guest in inv.people.filter(attendingWedding=True):
+                if attending_count == 4:
+                    continue
                 family.append(guest.first_name)
                 family.append(guest.last_name)
                 guest_count += 1
-                
+                attending_count += 1
             
+            missing_guests = 4 - attending_count
+            if missing_guests > 0:
+                for i in range(missing_guests):
+                    family.append('')
+                    family.append('')
+                
+            if attending_count == 0:
+                continue
             family.append(inv.name)
+            for i in range(7):
+                family.append('')
             families.append(family)
                     
         
